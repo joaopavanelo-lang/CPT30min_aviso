45ce796baed81b1f923961e1265f2e09cec8243c
joaopavanelo-lang <joao.pavanelo@shopee.com>
2026-03-05 20:41:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ TURNO_PARA_IDS = {
     ],
     "Turno 2": [
         "1458031670",  # Beatriz Quintiliano
-        "1189260998",  # Vitor Azeredo 
+        "1298055860",  # Matheus Damas
         "1281984509",  # Rafael Almeida
         "1432898616",  # Leonardo Caus
         
@@ -51,7 +51,7 @@ DIAS_DE_FOLGA = {
 
     # --- Turno 2 ---
     "1458031670": [6, 0],   # Murilo Santana (Dom, Seg)
-    "1189260998": [6],      # Vitor Azeredo (Dom) # joao cobrindo T2
+    "1298055860": [6],      # Mateus Damas
     "1281984509": [6],       # Rafael Almeida
     "1432898616": [4, 5],   # Leonardo Caus (Folga Seg a Sab)
 
