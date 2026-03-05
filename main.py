43896f3976c596dfac1d3609d5fb39c56b96df87
joaopavanelo-lang <joao.pavanelo@shopee.com>
2026-03-05 20:42:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ DIAS_DE_FOLGA = {
     # --- Turno 2 ---
     "1458031670": [6, 0],   # Murilo Santana (Dom, Seg)
     "1298055860": [6],      # Mateus Damas
-    "1281984509": [6],       # Rafael Almeida
+    "1281984509": [6],      # Rafael Almeida
     "1432898616": [4, 5],   # Leonardo Caus (Folga Seg a Sab)
 
     # --- Turno 3 ---
